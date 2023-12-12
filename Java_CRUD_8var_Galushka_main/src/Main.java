857ca8a2348b1e07abbb375dced9e11360f932e9
Miranaq <113391061+Miranaq@users.noreply.github.com>
2023-12-12 16:17:24 +0300

    Delete Java_CRUD_8var_Galushka_main directory

--- Java_CRUD_8var_Galushka_main/src/Main.java
+++
@@ -1,8 +0,0 @@
-import interfaceLine.CommandLineInterface;
-
-public class Main {
-    public static void main(String[] args) {
-        CommandLineInterface cli = new CommandLineInterface();
-        cli.start();
-    }
-}
